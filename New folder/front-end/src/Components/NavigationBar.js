61b500323a9e5850a9635fa87bef1a9af64499ba
Jatin Kumar Mahanta <121667012+jatinkumarmahanta@users.noreply.github.com>
2023-04-21 13:13:56 +0530

    Add files via upload

--- New folder/front-end/src/Components/NavigationBar.js
+++ New folder/front-end/src/Components/NavigationBar.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 
 const NavigationBar = () => {
+  const [expanded, setExpanded] = useState(false);
+
   const handleLogout = () => {
     localStorage.removeItem("authToken");
   }
-  return (
-    <Navbar className='bg-light' expand="lg">
-      <div className='d-flex me-auto'>
-        <Navbar.Brand as={Link} to="/">Laundry Management System</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {(localStorage.getItem("authToken")) ? <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-          <Nav.Link as={Link} to="/laundry-requests">Laundry Requests</Nav.Link>
-        </Nav> : <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-        </Nav>}
-      </div>
-      <Nav>
-        {!(localStorage.getItem("authToken")) ?
-          <div className='d-flex mx-1'>
-            <Nav.Link as={Link} to="/login">Log In</Nav.Link>
-            <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
-          </div> : <div className='d-flex mx-1'> <Nav.Link as={Link} to="/request-status">Request Status</Nav.Link>
-            <Nav.Link as={Link} to="/" onClick={handleLogout}>logout</Nav.Link>
-          </div>
-        }
-      </Nav>
 
+  return (
+    <Navbar className='bg-light' expand="lg" expanded={expanded}>
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/">Laundry Management System</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {(localStorage.getItem("authToken")) ? <Nav className="me-auto">
+            <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+            <Nav.Link as={Link} to="/laundry-requests">Laundry Requests</Nav.Link>
+            <Nav.Link as={Link} to="/request-status">Request Status</Nav.Link>
+          </Nav> : <Nav className="me-auto">
+            <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+          </Nav>}
+          <Nav>
+            {!(localStorage.getItem("authToken")) ?
+              <div className='d-flex mx-1'>
+                <Nav.Link as={Link} to="/login">Log In</Nav.Link>
+                <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+              </div> : <div className='d-flex mx-1'>
+                <Nav.Link as={Link} to="/" onClick={handleLogout}>logout</Nav.Link>
+              </div>
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar >
   );
 }
